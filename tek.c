150de45973ea9471f95019549339d58b2d2fbeb4
Gabriel Sakali <gabedevs@gmail.com>
2025-04-23 15:03:57 +0300

    Disble carriage return

--- tek.c
+++ tek.c
@@ -16,7 +16,7 @@ void enableRawMode() {
 
     struct termios raw = orig_termios;
 
-    raw.c_iflag &= ~(IXON);
+    raw.c_iflag &= ~(IXON | ICRNL);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
     // TODO: Test leftover input effect in cygwin
